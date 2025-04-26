de33a2a4e964c7ee0977425b4c8080f7987e0683
MR-TAN <135997970+Tan-vai@users.noreply.github.com>
2025-04-26 20:38:12 +0600

    Update victim-ip.sh

--- victim-ip.sh
+++ victim-ip.sh
@@ -6,7 +6,7 @@ HOST='127.0.0.1'
 PORT='8080' 
 
 ## ANSI colors (FG & BG)
-RED="$(printf '\033[35;5;196m')"  GREEN="$(printf '\x1b[35;5;156m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+RED="$(printf '\033[38;5;196m')"  GREEN="$(printf '\x1b[38;5;156m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
 MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  GREEN="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
 REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
 MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  GREENBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
