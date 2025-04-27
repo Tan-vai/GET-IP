55fe5c2fb2017e135a1b0b94131f0424dca07bcd
MR-TAN <135997970+Tan-vai@users.noreply.github.com>
2025-04-27 21:10:18 +0600

    Update launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,13 +1,13 @@
 
 
 if [[ $(uname -o) == *'Android'* ]];then
-	Spider-VIrus_ROOT="/data/data/com.termux/files/usr/opt/Spider-VIrus"
+	Spider-Virus_ROOT="/data/data/com.termux/files/usr/opt/Spider-Virus"
 else
-	export Spider-VIrus_ROOT="/opt/Spider-VIrus"
+	export Spider-Virus_ROOT="/opt/Spider-Virus"
 fi
 
 if [[ $1 == '-h' || $1 == 'help' ]]; then
-	echo "To run Spider-VIrus type \`Spider-VIrus\` in your cmd"
+	echo "To run Spider-Virus type \`Spider-Virus\` in your cmd"
 	echo
 	echo "Help:"
 	echo " -h | help : Print this menu & Exit"
@@ -16,11 +16,11 @@ if [[ $1 == '-h' || $1 == 'help' ]]; then
 	echo
 
 elif [[ $1 == '-i' || $1 == 'ip' ]]; then
-	cat $Spider-VIrus_ROOT/auth/ip.txt 2> /dev/null || {
+	cat $Spider-Virus_ROOT/auth/ip.txt 2> /dev/null || {
 		echo "No Saved IP Found !"
 		exit 1
 	}
 else
-	cd $Spider-VIrus_ROOT
+	cd $Spider-Virus_ROOT
 	bash ./spider.sh
 fi
